6da1d771f4fc68384afa3433110b67a097e70edc
Alex Davis <acdavis050@gmail.com>
2020-07-29 19:22:03 -0700

    Working on music syncing functionality

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = 'f027df0d4399439bb2e13a2aebee6a4b'; // Your client id
+var client_secret = 'b4ebbc598a4f466fac41e6a4d3ecadba'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
