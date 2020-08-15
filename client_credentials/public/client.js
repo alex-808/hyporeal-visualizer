72cfded1bb07c25291f5c1748e6cf2382c531da0
Alex Davis <acdavis050@gmail.com>
2020-08-14 21:54:53 -0700

    Various major updates

--- client_credentials/public/client.js
+++ client_credentials/public/client.js
@@ -1,4 +1,4 @@
-
+import {GLTFLoader} from "https://unpkg.com/three@0.119.1/examples/jsm/loaders/GLTFLoader.js"
 
 window.onSpotifyWebPlaybackSDKReady = () => {
 
@@ -10,8 +10,14 @@ var planeStoredBackRow = [0,0,0,0,0,0,0,0,0,0,0,0]
 
 var neoDimension
 
+var neoLineXVector = 0
+var neoLineYVector = 0
+
+var pitchSum
 
 function main() {
+
+
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({canvas, alpha: true, antialias: true});
   
@@ -88,7 +94,7 @@ function main() {
     function setupScene3() {
       //Pen
       const sceneInfo = makeScene(document.querySelector('#plane'));
-      const radius = .02;
+      const radius = .2;
       const widthSegments = 1
       const heightSegments = 1
       const geometry = new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments);
@@ -104,10 +110,15 @@ function main() {
       sceneInfo.scene.add(pen);
       sceneInfo.pen = pen;
 
+      var planeWidth = 1.5
+      var planeHeight = 1.5
+
       //Plane
-      var planeGeometry = new THREE.PlaneGeometry( 1.5, 1.5, neoDimension, neoDimension);
+      var planeGeometry = new THREE.PlaneGeometry( planeWidth, planeHeight, neoDimension, neoDimension);
       var planeMaterial = new THREE.MeshBasicMaterial();
       var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+      sceneInfo.planeWidth = planeWidth
+      sceneInfo.planeHeight = planeHeight
       sceneInfo.scene.add(plane);
       plane.material.wireframe = true
       plane.rotation.set(0, 0, 0)
@@ -136,7 +147,7 @@ function main() {
       neoLinePoints.push( new THREE.Vector3( pen.position.x, pen.position.y, 0 ) );
       neoLinePoints.push( new THREE.Vector3( pen.position.x, pen.position.y, 0 ) );
       var neoLineArray = []
-      var neoLineMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff, } );
+      var neoLineMaterial = new THREE.LineBasicMaterial( { color: 'white', } );
       var neoLineGeometry = new THREE.BufferGeometry().setFromPoints(neoLinePoints)
       var neoLine = new THREE.Line(neoLineGeometry, neoLineMaterial)
       sceneInfo.neoLine = neoLine
@@ -149,55 +160,72 @@ function main() {
     var neoLineCounter = 0
     var writeCounter = 0
     var toggle = true
-    var storedBeatCounter = 0
+    var neoStoredBarCounter = 0
     var toggle2 = true
     var r = .3
 
+    var neoLineVelocity = 0.1
+    var cameraMovement = .001
+  console.log(trackData)
+
   function neoLineAnimation() {
-    if (storedBeatCounter !== beatCounter) {
-      storedBeatCounter = beatCounter
-      sceneInfo3.pen.position.x = squaresArray[barCounter][0].x
-      sceneInfo3.pen.position.y = squaresArray[barCounter][0].y
-      sceneInfo3.neoLinePoints.shift()
-      sceneInfo3.neoLinePoints.shift()
-      sceneInfo3.neoLinePoints.push( new THREE.Vector3( sceneInfo3.pen.position.x, sceneInfo3.pen.position.y, 0 ) );
-      sceneInfo3.neoLinePoints.push( new THREE.Vector3( sceneInfo3.pen.position.x, sceneInfo3.pen.position.y, 0 ) );
-
-      drawTriangle(sceneInfo3.pen.position.x, sceneInfo3.pen.position.y)
-
-    }
-   
-    //Trying to draw a circle
-    // if(toggle2 === true) {
-    //   for (var x = -r; x <= r;) {
-    //         console.log("X", x)
-    //         x = parseFloat((x += .01).toFixed(1))
-    //     for (var y = -r; y <= r;) {
-    //           console.log("Y", y)
-    //           y = parseFloat((y += .01).toFixed(1))
-    //       if(Math.pow(x, 2) + Math.pow(y, 2) === Math.pow(r, 2)) {
-    //         sceneInfo3.pen.position.x = x
-    //         sceneInfo3.pen.position.y = y
-    //         drawNeoLine()
-    //         console.log("X", x)
-    //         console.log("Y", y)
-    //         console.log("X squared", Math.pow(x, 2))
-    //         console.log("Y squared", Math.pow(y, 2))
-    //         // console.log("The sum", Math.pow(x, 2) + Math.pow(y, 2))
-    //         // console.log("The radius squared", Math.pow(r, 2))
-    //       }
-    //     }
-    //   }
-
-    //   toggle2 = false
-    //   console.log('ran')
-    // }
-    drawNeoLine()
+    //neoLineCounter++
+    //if (neoLineCounter === 10) {
+      //neoLineCounter = 0
+      if (neoStoredBarCounter !== barCounter) {
+        console.log('ran')
+        neoStoredBarCounter = barCounter
+        cameraMovement = -cameraMovement
+        sceneInfo3.pen.position.x = squaresArray[barCounter][0].x + .001
+        sceneInfo3.pen.position.y = squaresArray[barCounter][0].y - sceneInfo3.planeWidth/neoDimension/2
+        sceneInfo3.pen.position.z = 0
+        sceneInfo3.neoLinePoints.shift()
+        sceneInfo3.neoLinePoints.shift()
+        sceneInfo3.neoLinePoints.push( new THREE.Vector3( sceneInfo3.pen.position.x, sceneInfo3.pen.position.y, sceneInfo3.pen.position.z ) );
+        sceneInfo3.neoLinePoints.push( new THREE.Vector3( sceneInfo3.pen.position.x, sceneInfo3.pen.position.y, sceneInfo3.pen.position.z ) );
   
-    if (toggle === true) {
-      console.log(sceneInfo3.neoLine.geometry)
-      toggle = false
-    }
+        //drawTriangle(sceneInfo3.pen.position.x, sceneInfo3.pen.position.y)
+      }
+      //sceneInfo3.camera.position.x += cameraMovement
+      //sceneInfo3.camera.position.y += cameraMovement
+
+      // if (neoLineXVector > 10) {
+      //   neoLineXVector = 0
+      // }
+      if (neoLineYVector > 0) {
+        neoLineYVector -= 0.01
+      }
+      else if (neoLineYVector < 0) {
+        neoLineYVector += 0.01
+      }
+
+      if (neoLineXVector > 0) {
+        neoLineXVector -= 0.1
+      }
+      else if (neoLineXVector < 0) {
+        neoLineXVector += 0.1
+      }
+      sceneInfo3.pen.position.y += neoLineVelocity * neoLineYVector
+      sceneInfo3.pen.position.x += neoLineVelocity * neoLineXVector
+      if (sceneInfo3.pen.position.x < squaresArray[barCounter][0].x || sceneInfo3.pen.position.x > squaresArray[barCounter][1].x) {
+        //console.log("Hit")
+        neoLineXVector = neoLineXVector * -1
+      }
+
+      if (sceneInfo3.pen.position.y > squaresArray[barCounter][0].y || sceneInfo3.pen.position.y < squaresArray[barCounter][2].y) {
+        //console.log("hot")
+        neoLineYVector = neoLineYVector * -1
+      }
+   
+      drawNeoLine()
+    
+      if (toggle === true) {
+        //console.log(sceneInfo3.neoLine.geometry)
+        toggle = false
+      }
+
+    //}
+
   }
 
   function drawTriangle(x, y) {
@@ -210,30 +238,176 @@ function main() {
     drawNeoLine()
     sceneInfo3.pen.position.x = initialX
     sceneInfo3.pen.position.y = initialY
-    //drawNeoLine()
+
+  }
+
+  function drawCircle (x, y) {
+
+        //Trying to draw a circle
+
+      for (var x = -r; x <= r;) {
+            console.log("X", x)
+            x = parseFloat((x += .01).toFixed(1))
+        for (var y = -r; y <= r;) {
+              console.log("Y", y)
+              y = parseFloat((y += .01).toFixed(1))
+          if(Math.pow(x, 2) + Math.pow(y, 2) === Math.pow(r, 2)) {
+            sceneInfo3.pen.position.x = x
+            sceneInfo3.pen.position.y = y
+            drawNeoLine()
+            console.log("X", x)
+            console.log("Y", y)
+            console.log("X squared", Math.pow(x, 2))
+            console.log("Y squared", Math.pow(y, 2))
+            // console.log("The sum", Math.pow(x, 2) + Math.pow(y, 2))
+            // console.log("The radius squared", Math.pow(r, 2))
+          }
+        }
+      }
+
+ 
   }
 
   function drawNeoLine () {
 
     sceneInfo3.neoLinePoints.shift()
-    sceneInfo3.neoLinePoints.push( new THREE.Vector3( sceneInfo3.pen.position.x, sceneInfo3.pen.position.y, 0 ) );
+    sceneInfo3.neoLinePoints.push( new THREE.Vector3( sceneInfo3.pen.position.x, sceneInfo3.pen.position.y, sceneInfo3.pen.position.z ) );
 
     sceneInfo3.neoLine.geometry = new THREE.BufferGeometry().setFromPoints(sceneInfo3.neoLinePoints)
     sceneInfo3.neoLineArray[lineCounter] = new THREE.Line(sceneInfo3.neoLine.geometry, sceneInfo3.neoLine.material)
     neoLineCounter++
     sceneInfo3.scene.add(sceneInfo3.neoLineArray[lineCounter])
   }
-
+  
+    var scene4Toggle = false
     function setupScene4() {
+
       const sceneInfo = makeScene(document.querySelector('#dodecahedron'));
-      const geometry = new THREE.DodecahedronBufferGeometry(1, 1, 1);
-      const material = new THREE.MeshPhongMaterial({color: 'red'});
-      const mesh = new THREE.Mesh(geometry, material);
-      sceneInfo.scene.add(mesh);
-      sceneInfo.mesh = mesh;
-      sceneInfo.camera.position.z = 4
+
+      var dimensions = 5
+      var bufferTexture = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter});
+      var bufferScene = new THREE.Scene();
+      var bufferLight = new THREE.DirectionalLight(0xffffff, 0.5 )
+      bufferScene.add(bufferLight)
+      //bufferScene.background = new THREE.Color('white')
+
+      var geometry = new THREE.BoxGeometry(1, 1, 1);
+      var material = new THREE.MeshBasicMaterial( { color: 'blue' } );
+      var bufferCube = new THREE.Mesh( geometry, material );
+      sceneInfo.bufferCube = bufferCube
+      sceneInfo.bufferTexture = bufferTexture
+      sceneInfo.bufferScene = bufferScene
+      //bufferScene.add(bufferCube)
+
+      var geometry = new THREE.PlaneGeometry(dimensions, dimensions , 100, 100);
+      var material = new THREE.MeshBasicMaterial( {map: bufferTexture});
+      var plane = new THREE.Mesh( geometry, material );
+      plane.position.z = -1
+      sceneInfo.scene.add( plane );
+      sceneInfo.plane = plane
+
+
+      var sculpture
+      const loader = new GLTFLoader()
+      loader.load("./Sculpture2.gltf", (object) => {
+        sculpture = object.scene.children[2]
+
+        sculpture.rotation.x = 9.5
+        sculpture.position.x = 0
+        sculpture.position.y = 5
+        sculpture.position.z = -10
+        sculpture.scale.x = 0.1
+        sculpture.scale.y = 0.1
+        sculpture.scale.z = 0.1
+        sceneInfo.sculpture = sculpture
+        sceneInfo.bufferScene.add(sculpture)
+        scene4Toggle = true
+      })
+
+      console.log(plane)
+      //we are trying to make the sculpture accessible for manipulation outside of the load function
+      
       return sceneInfo;
+
     }
+    var factor = -1.5
+    var vector = 0.1
+    var range = .1
+    var shift = .01
+    function distortObject(object) {
+
+      if (factor + range > 1.5) {
+          vector = -.1
+          shift = -.01
+          //console.log('Tic')
+          //vector2 = -0.01
+      }
+      else if (factor - range < -1.5) {
+          vector = .1
+          shift = .01
+          //console.log('Toc')
+      }
+      else {
+          for (var i = 0; i < object.geometry.vertices.length; i++) {
+
+              if (object.geometry.vertices[i].y > factor - range && object.geometry.vertices[i].y < factor + range ) {
+
+                  object.geometry.vertices[i].x += shift
+              }
+      
+          } 
+      }
+      
+      factor += vector
+
+      //console.log(factor)
+  }
+  var glitchPoint = 0
+  var glitchRange = 1
+  var glitchAmount = .1
+  var glitchOriginalArray = []
+  var glitchInterval
+  var glitchStoredBarCounter
+  function glitchDistort (object) {
+    
+    if (glitchStoredBarCounter !== barCounter) {
+      glitchStoredBarCounter = barCounter
+      //console.log('ran')
+      glitchOriginalArray = object.geometry.vertices
+      for (var i = 0; i < object.geometry.vertices.length; i++) {
+
+        if (object.geometry.vertices[i].y > glitchPoint - glitchRange && object.geometry.vertices[i].y < glitchPoint + glitchRange ) {
+
+            object.geometry.vertices[i].x += glitchAmount
+        }
+
+        object.geometry.verticesNeedUpdate = true
+
+      }
+      glitchInterval = setInterval( function () {
+        resetGlitchDistort(object)
+      }, 200)
+
+    }
+
+
+  }
+  function resetGlitchDistort(object) {
+    //console.log('run')
+    clearInterval(glitchInterval)
+    for (var i = 0; i < object.geometry.vertices.length; i++) {
+      
+      if (object.geometry.vertices[i].y > glitchPoint - glitchRange && object.geometry.vertices[i].y < glitchPoint + glitchRange ) {
+
+        object.geometry.vertices[i].x -= glitchAmount
+    }
+          
+      }
+      object.geometry.verticesNeedUpdate = true
+
+  }
+
+
   const sceneInfo1 = setupScene1();
   const sceneInfo2 = setupScene2();
   const sceneInfo3 = setupScene3();
@@ -252,6 +426,7 @@ function main() {
   }
 
   function renderSceneInfo(sceneInfo) {
+
     const {scene, camera, elem} = sceneInfo;
 
     // get the viewport relative position of this element
@@ -275,6 +450,13 @@ function main() {
     renderer.setScissor(left, positiveYUpBottom, width, height);
     renderer.setViewport(left, positiveYUpBottom, width, height);
 
+    if(sceneInfo === sceneInfo4) {
+      sceneInfo4.plane.geometry.verticesNeedUpdate = true
+      renderer.setRenderTarget(sceneInfo4.bufferTexture)
+      renderer.render(sceneInfo4.bufferScene, camera)
+      renderer.setRenderTarget(null)
+    }
+
     renderer.render(scene, camera);
   }
 
@@ -284,13 +466,13 @@ function main() {
 
     if (planeStoredBackRow !== planeBackRowAnimation) {
       planeStoredBackRow = planeBackRowAnimation
-      for(i = 0; i < planeDimension; i++) {
+      for(var i = 0; i < planeDimension; i++) {
         sceneInfo1.mesh.geometry.vertices[i].z = planeStoredBackRow[i]
       }
     }
 
     else {
-      for(i = 0; i < planeDimension; i++) {
+      for(var i = 0; i < planeDimension; i++) {
         if(sceneInfo1.mesh.geometry.vertices[i].z > 0) {
           sceneInfo1.mesh.geometry.vertices[i].z -= 0.01
         }
@@ -357,23 +539,26 @@ function main() {
     pitchPlaneAnimation()
     beatLineAnimation()
     neoLineAnimation()
+    glitchDistort(sceneInfo4.plane)
 
+    if(scene4Toggle === true) {
+      sceneInfo4.sculpture.rotation.y += .01
+      distortObject(sceneInfo4.plane)
+    }
+    
     resizeRendererToDisplaySize(renderer);
 
     renderer.setScissorTest(false);
     renderer.clear(true, true);
     renderer.setScissorTest(true);
 
-
-
-
-    sceneInfo4.mesh.rotation.y = time * .1;
-
     renderSceneInfo(sceneInfo1);
     renderSceneInfo(sceneInfo2);
     renderSceneInfo(sceneInfo3);
+    renderer.setRenderTarget(sceneInfo4.bufferTexture)
     renderSceneInfo(sceneInfo4);
-
+    renderer.setRenderTarget(null)
+    renderSceneInfo(sceneInfo4);
     requestAnimationFrame(render);
   }
 
@@ -440,6 +625,7 @@ function setSquaresArray (bars) {
           beatAnimation = -1
           //console.log("Boop")
         }
+
         beatCounter++
       }
                
@@ -459,12 +645,24 @@ function setSquaresArray (bars) {
 
      if (elapsedTime + elapsedMilliseconds >= trackData.segmentsStart[segmentCounter] - syncCompensation) {
       planeBackRowAnimation = trackData.segments[segmentCounter]['pitches']
+      //console.log(planeBackRowAnimation)
       //console.log(trackData.segmentsStart[segmentCounter])
       //console.log(backRowAnimation)
-
+      pitchSum = trackData.segments[segmentCounter].pitches.reduce(timbreReducer)
+      //console.log(pitchSum)
+      //pitchParsed = (pitchSum - 6) / 12
+      //console.log(pitchParsed)
+/
+      //trackData.segments[segmentCounter].timbre
       segmentCounter++
       //console.log("Segment hit", segmentCounter)
      }
+    }
+
+    function timbreReducer (acc, val) {
+
+      return acc += val
+
     }
     function timerControl(paused) {
 
@@ -507,7 +705,7 @@ function setSquaresArray (bars) {
         clearInterval(id)
       }
 
-    const token = 'BQDjtmyJPSj9eBeyFg7QDtVmYcW84fr86vP-K9SypN1fwLszAcORwX-WfP5BYfTh-JY_DWJiQeDa-P4wvZdE_HMGHoIqv7bGc01VgIZ47dkxdiyQLkhmkN0g1gm4ro0XQ3-4YAbfny0Uf4KMCEQwUl5U4SOMsw5dIQ';
+    const token = 'BQD_I6bDXeGP45lvMLiQchdWDDPcODAn1s_XcRLQvdP_a_HH6s-UifuXL9Z4IoCYhxorigliftPgtEY3I1PNL9OA-T5lND_KkhVdM7UlUYAkWwBrUs4v7p9elUUBFfMvGqrsin5GDJ9cdBNPPG8ZTBeDpcLVN1yFBQ';
     const player = new Spotify.Player({
       name: 'Hyporeal',
       getOAuthToken: cb => { cb(token); }
@@ -559,7 +757,7 @@ function setSquaresArray (bars) {
           timerControl(state['paused'])
         }
     });
-var trackPositionQueryRate = 10000
+var trackPositionQueryRate = 1000
 var trackPositionQuery
 //add something to allow stop querying when we don't need to
     trackPositionQuery = setInterval(() => {
